dfc82b0956171e7b268d74b7c3a26e3ad9128b9e
Selim <selimchemutai19@gmail.com>
2021-09-14 21:30:35 +0300

    update on main

--- Control_Timer/main.c
+++ Control_Timer/main.c
@@ -1,18 +1,32 @@
-/*
- * Control_Timer.c
- *
- * Created: 14/09/2021 13:05:31
- * Author : Default
- */ 
+#include <avr/io.h> void timer_delay(); 
+int main(void){ DDRC = 0xFF; /* PORTC as output*/ /* main loop */
 
-#include <avr/io.h>
+ while(1){ 
+     
+     PORTC=0x01; /* Pulse PC1 ON*/
 
+  timer_delay(); /* 20us delay */ 
 
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
-}
+  PORTC=0x00; /* Pulse PC1 OFF */ 
+
+  timer_delay(); /* 20us delay */ }
+   }
+   void timer_delay(){
+
+        TCNT0 = 0x5F; 
+        /* Load TCNT0 with the start value*/
+
+         TCCR0 = 0x01; 
+         /* Timer0, normal mode, no pre-scalar */ 
+
+         while((TIFR&0x01)==0);
+          /* Wait for TOV0 to roll over, i.e. overflow */
+
+         TCCR0 = 0;
+          /* set TCCR0 to 0 to stop the timer */ 
+
+         TIFR = 0x1;
+          /* Clear TOV0 flag*/
+
+          }
 
