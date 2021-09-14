9db7b9a364921612ead9e9f862f128b16e9fe10a
Selim <selimchemutai19@gmail.com>
2021-09-14 21:27:49 +0300

    main.c

---
+++ Control_Timer/main.c
@@ -0,0 +1,18 @@
+/*
+ * Control_Timer.c
+ *
+ * Created: 14/09/2021 13:05:31
+ * Author : Default
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
